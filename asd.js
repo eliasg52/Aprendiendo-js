9711a6957d77cb52ec766b40ba9d7fbe08d57911
eliasg52 <eliasgarcia81@gmail.com>
2020-05-20 20:11:34 -0300

    Js
    Aprendiendo JS

---
+++ asd.js
@@ -0,0 +1,9 @@
+var precioDolar, cantidad, total;
+
+precioDolar=58;
+
+cantidad=100;
+
+total=precioDolar * cantidad; 
+
+alert("En total son " + total + " pesos");
---
+++ primerEjemplo/primerejemplo.js
@@ -0,0 +1,1 @@
+alert("Archivo js linkeado");
